1c9168696d32eba60153fff77c4f55cc1b99b28f
gonmpr <gmporrorosas@gmail.com>
2026-07-15 22:04:36 +0000

    checking freq in each part

--- main.c
+++ main.c
@@ -8,32 +8,51 @@
 typedef unsigned long long u64;
 
 int main(void){
+  u64 program_start_timer = read_cpu_clock();
   
   pair_t *pairs = NULL;
   size_t count = 0;
-
-  u64 time_start = read_cpu_clock();
+  
+  u64 parser_start_timer = read_cpu_clock(); 
 
   if (!load_pairs_file("input/input.json", &pairs, &count)) {
       printf("could not load pairs\n");
       return 1;
   }
 
+  u64 parser_end_timer = read_cpu_clock(); 
+
+
+  u64 haversine_start_timer = read_cpu_clock();
   for (size_t i = 0; i < count; i++) {
-      printf(
-          "x0:%f y0:%f -> x1:%f y1:%f | ",
-          pairs[i].x0,
-          pairs[i].y0,
-          pairs[i].x1,
-          pairs[i].y1
-      );
-      printf("d: %f\n", reference_haversine(pairs[i]));
+      reference_haversine(pairs[i]);
   }
 
-  u64 time_end = read_cpu_clock();
+  u64 haversine_end_timer = read_cpu_clock();
 
-  printf("CPU FREQUENCY: %llu Hz\n", get_cpu_freq());
-  printf("CYCLES: %llu\n", time_end - time_start);
+
+  u64 start_time = parser_start_timer - program_start_timer;
+  u64 parsing_time = parser_end_timer - parser_start_timer;
+  u64 haverine_time = haversine_end_timer - haversine_start_timer;
+
+  u64 program_end_timer = read_cpu_clock();
+
+  u64 total_time = program_end_timer - program_start_timer;
+  u64 ending_time =  program_end_timer - haversine_end_timer;
+
+  printf("\nCPU FREQUENCY: %llu Hz\n", get_cpu_freq());
+  printf("TOTAL: %llu Hz (100%%) \n\n", total_time);
+
+  printf("STARTING: %llu Hz (%f%%) \n",
+      start_time, (double)start_time/total_time * 100);
+
+  printf("PARSING: %llu Hz (%f%%) \n", 
+      parsing_time, (double)parsing_time/total_time * 100);
+
+  printf("HAVERINE: %llu Hz (%f%%) \n",
+      haverine_time, (double)haverine_time/total_time * 100);
+  printf("ENDING: %llu Hz (%f%%) \n", 
+      ending_time, (double)ending_time/total_time * 100);
 
   free(pairs);
 }
